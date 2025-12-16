86d5c4fa0f2f8cfe474a1893ffb912a6dfe96c25
dgqnue <dgqnue@gmail.com>
2025-12-16 15:10:29 +0800

    修复加载页面黑屏

--- client/src/games/chinesechess/gamepagehierarchy/ChineseChessDisplayPlugin.tsx
+++ client/src/games/chinesechess/gamepagehierarchy/ChineseChessDisplayPlugin.tsx
@@ -665,7 +665,7 @@ function ChineseChessDisplay({ tableClient, isMyTable, onLeaveTable }: ChineseCh
           className="absolute inset-0 z-50 flex flex-col items-center justify-center"
           style={{
             backgroundColor: '#000', // Ensure opaque background
-            backgroundImage: 'url("/images/chinesechess/ui/loadPageBackground.png?v=' + Date.now() + '")',
+            backgroundImage: 'url("/images/chinesechess/ui/loadPageBackground.png")',
             backgroundSize: '100% auto',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat'
